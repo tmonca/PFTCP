5e54f0bd02b8d2a4024f44f62becc7eac011b7bf
Toby <toby@nf-test112.cl.cam.ac.uk>
2012-08-13 14:33:21 +0100

    out=TCP, rtn=UDP

--- cclient.c
+++ cclient.c
@@ -5,57 +5,91 @@
 #include <string.h>
 #include <unistd.h>
 #include <netinet/in.h>
-#define BUFFSIZE 32
+#define BUFFSIZE 64  /*A compromise length*/
 
 void Die(char *mess) { perror(mess); exit(1); }
 
 int main(int argc, char *argv[]) {
-	int sock;
-	struct sockaddr_in echoserver;
-	char buffer[BUFFSIZE];
-	unsigned int echolen;
-	int received = 0;
+	/*variables for TCP*/
+	int sock_tcp;
+	struct sockaddr_in tcp_echoserver;
+	char out_buffer[BUFFSIZE];
+	unsigned int tcp_echolen;
+
 	
+	/*variables for the UDP listener*/
+  int sock_udp;
+  struct sockaddr_in udp_echoserver;
+  struct sockaddr_in udp_echoclient;
+  char reply_buffer[BUFFSIZE];
+  unsigned int udp_echolen, clientlen, serverlen;
+  int received = 0;
+  int j;
+  
+	/*Check for correct usage*/
 	if (argc != 4) {
   		fprintf(stderr, "USAGE: TCPecho <server_ip> <word> <port>\n");
   		exit(1);
 	}
 
 /* Create the TCP socket */
-	if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
-  		Die("Failed to create socket");
+	if ((sock_tcp = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+  		Die("Failed to create TCP socket");
   	}
-
-/* Construct the server sockaddr_in structure */
-	memset(&echoserver, 0, sizeof(echoserver));
-	echoserver.sin_family = AF_INET;
-	echoserver.sin_addr.s_addr = inet_addr(argv[1]);
-	echoserver.sin_port = htons(atoi(argv[3]));
-
-/* Establish connection */
-	if (connect(sock, (struct sockaddr *) &echoserver, sizeof(echoserver)) < 0) {
-  		Die("Failed to connect with server");
+/* Create the UDP socket */
+	if ((sock_udp = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+  	Die("Failed to create UDP socket");
 	}
+
+	/* Construct the UDP server sockaddr_in structure */
+	memset(&udp_echoserver, 0, sizeof(udp_echoserver));       /* Clear struct */
+	udp_echoserver.sin_family = AF_INET;                  /* Internet/IP */
+	udp_echoserver.sin_addr.s_addr = inet_addr("127.0.0.1");   /* Any IP address */
+	/*udp_echoserver.sin_port = htons(atoi(argv[3])+1);        server port */
+	udp_echoserver.sin_port = htons(13);
+		
+	serverlen = sizeof(udp_echoserver);
+	if (bind(sock_udp, (struct sockaddr *) &udp_echoserver, serverlen) < 0) {
+  	Die("Failed to bind UDP server socket");
+	} 
+
+	/* Construct the TCP server sockaddr_in structure */
+	memset(&tcp_echoserver, 0, sizeof(tcp_echoserver));
+	tcp_echoserver.sin_family = AF_INET;
+	tcp_echoserver.sin_addr.s_addr = inet_addr(argv[1]);
+	tcp_echoserver.sin_port = htons(atoi(argv[3]));
 	
+	/* Bind the UDP socket */
+
+
+	/* Establish TCP connection */
+	if (connect(sock_tcp, (struct sockaddr *) &tcp_echoserver, sizeof(tcp_echoserver)) < 0) {
+  		Die("Failed to connect with TCP server");
+	}
 /* Send the word to the server */
-	echolen = strlen(argv[2]);
-	if (send(sock, argv[2], echolen, 0) != echolen) {
+	tcp_echolen = strlen(argv[2]);
+	if (send(sock_tcp, argv[2], tcp_echolen, 0) != tcp_echolen) {
   		Die("Mismatch in number of sent bytes");
 	}
 	
 /* Receive the word back from the server */
 	fprintf(stdout, "Received: ");
-	while (received < echolen) {
-  		int bytes = 0;
-  		if ((bytes = recv(sock, buffer, BUFFSIZE-1, 0)) < 1) {
-    		Die("Failed to receive bytes from server");
-  		}
-  		received += bytes;
-  		buffer[bytes] = '\0';        /* Assure null terminated string */
-  		fprintf(stdout, buffer);
+	
+	while (received < tcp_echolen) {
+
+  	/*The following lines listen on the UDP socket for the reply*/
+  	clientlen = sizeof(udp_echoclient);
+    if ((received = recvfrom(sock_udp, reply_buffer, BUFFSIZE, 0, (struct sockaddr *) &udp_echoclient, &clientlen)) < 0) {
+      Die("Failed to receive message");
+    }
+    fprintf(stderr, "Client connected: %s\n", inet_ntoa(udp_echoclient.sin_addr));
+  		
+  	reply_buffer[received] = '\0';        /* Assure null terminated string */
+  	fprintf(stdout, reply_buffer);
 	}
   	fprintf(stdout, "\n");
-  	close(sock);
+  	close(sock_udp);
+  	close(sock_tcp);
   	exit(0);
 }
 
--- cserver.c
+++ cserver.c
@@ -7,80 +7,88 @@
 #include <netinet/in.h>
 #include <math.h>
 #define MAXPENDING 5    /* Max connection requests */
-#define BUFFSIZE 32
+#define BUFFSIZE 64
 
 void Die(char *mess) { perror(mess); exit(1); }
 
-void HandleClient(int sock) {
+void HandleClient(int rcvsock) {
   char buffer[BUFFSIZE];
-  char reply[BUFFSIZE];
   char sum, tmp, tmp2;
-int i;
+	int udp_clientsock;
+	struct sockaddr_in udp_echoserver;
   int received = -1;
   /* Receive message */
-  if ((received = recv(sock, buffer, BUFFSIZE, 0)) < 0) {
+  if ((received = recv(rcvsock, buffer, BUFFSIZE, 0)) < 0) {
     Die("Failed to receive initial bytes from client");
   }
-/* Calculate a "checksum" and send that back instead 
-
-Start with something simple 
-*/
-  reply[received] = buffer[received];
-  for (i = 0; i < received; i++) {
-    tmp = buffer[i];
-    fprintf(stderr, "char is %c\n", tmp);
-    reply[received -1 - i] = tmp+2;
-   }
-
-  /* Send bytes and check for more incoming data in loop */
+  fprintf(stderr, "Received %s \n", buffer);
+  
+  /* Create the UDP socket */
+	if ((udp_clientsock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+  	Die("Failed to create socket");
+	}
+/* Construct the UDP server sockaddr_in structure */
+	memset(&udp_echoserver, 0, sizeof(udp_echoserver));       /* Clear struct */
+	udp_echoserver.sin_family = AF_INET;                  /* Internet/IP */
+	udp_echoserver.sin_addr.s_addr = inet_addr("127.0.0.1");   /* Any IP address */
+	udp_echoserver.sin_port = htons(13);  								/*hard wire the UDP to port 13*/
+  
   while (received > 0) {
     /* Send back received data */
-    if (send(sock, reply, received, 0) != received) {
-      Die("Failed to send bytes to client");
-    }
+    if (sendto(udp_clientsock, buffer, received, 0, (struct sockaddr *) &udp_echoserver, sizeof(udp_echoserver)) != received) {
+  	Die("Failed to send UDP reply");
+	}
     /* Check for more data */
-    if ((received = recv(sock, buffer, BUFFSIZE, 0)) < 0) {
+    if ((received = recv(rcvsock, buffer, BUFFSIZE, 0)) < 0) {
       Die("Failed to receive additional bytes from client");
     }
   }
-  close(sock);
+  close(rcvsock);
+  close(udp_clientsock);
 }
 
 int main(int argc, char *argv[]) {
-  int serversock, clientsock;
-  struct sockaddr_in echoserver, echoclient;
+  int tcp_serversock, tcp_clientsock;
+  struct sockaddr_in tcp_echoserver, tcp_echoclient;
   if (argc != 2) {
     fprintf(stderr, "USAGE: echoserver <port>\n");
     exit(1);
   }
   /* Create the TCP socket */
-  if ((serversock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+  if ((tcp_serversock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
     Die("Failed to create socket");
   }
-  /* Construct the server sockaddr_in structure */
-  memset(&echoserver, 0, sizeof(echoserver));       /* Clear struct */
-  echoserver.sin_family = AF_INET;                  /* Internet/IP */
-  echoserver.sin_addr.s_addr = htonl(INADDR_ANY);   /* Incoming addr */
-  echoserver.sin_port = htons(atoi(argv[1]));       /* server port */
+  /* Construct the TCP server sockaddr_in structure */
+  memset(&tcp_echoserver, 0, sizeof(tcp_echoserver));       /* Clear struct */
+  tcp_echoserver.sin_family = AF_INET;                  /* Internet/IP */
+  tcp_echoserver.sin_addr.s_addr = inet_addr("127.0.0.1");   /* Incoming addr */
+  tcp_echoserver.sin_port = htons(atoi(argv[1]));       /* server port */
 
 /* Bind the server socket */
-	if (bind(serversock, (struct sockaddr *) &echoserver, sizeof(echoserver)) < 0) {
- 		Die("Failed to bind the server socket");
+	if (bind(tcp_serversock, (struct sockaddr *) &tcp_echoserver, sizeof(tcp_echoserver)) < 0) {
+ 		Die("Failed to bind the TCP server socket");
 	}
+
+/* Bind the socket 
+	if (bind(udp_serversock, (struct sockaddr *) &udp_echoserver, sizeof(udp_echoserver)) < 0) {
+  	Die("Failed to bind UDP server socket");
+	}*/
+	
 /* Listen on the server socket */
-	if (listen(serversock, MAXPENDING) < 0) {
+	if (listen(tcp_serversock, MAXPENDING) < 0) {
   	Die("Failed to listen on server socket");
 	}
 
   /* Run until cancelled */
   while (1) {
-    unsigned int clientlen = sizeof(echoclient);
+    unsigned int clientlen = sizeof(tcp_echoclient);
     /* Wait for client connection */
-    if ((clientsock = accept(serversock, (struct sockaddr *) &echoclient, &clientlen)) < 0) {
+    if ((tcp_clientsock = accept(tcp_serversock, (struct sockaddr *) &tcp_echoclient, &clientlen)) < 0) {
       Die("Failed to accept client connection");
     }
-    fprintf(stdout, "Client connected: %s\n", inet_ntoa(echoclient.sin_addr));
-    HandleClient(clientsock);
+    fprintf(stdout, "Client connected: %s\n", inet_ntoa(tcp_echoclient.sin_addr));
+    
+    HandleClient(tcp_clientsock);
   }
 }
 
